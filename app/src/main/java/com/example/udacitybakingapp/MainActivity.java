3b09fda5a1dac30382214b0e57cd093f0a2eb2de
Martin PFEIFLE <martin.pfeifle@nng.com>
2021-01-06 18:36:11 +0100

    first commit for RecyclerViewTest

---
+++ app/src/main/java/com/example/udacitybakingapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.udacitybakingapp;
+
+import android.os.Bundle;
+import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.udacitybakingapp.adapter.AdapterMainActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rv_mainActivity;
+    // cf. https://developer.android.com/guide/topics/ui/layout/recyclerview
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv_mainActivity =findViewById(R.id.rv_mainActivity);
+
+        Context context = rv_mainActivity.getContext();
+        String test[] = {"1","2","3","4","5"};
+        AdapterMainActivity adapterMainActivity = new AdapterMainActivity(test);
+        rv_mainActivity.setAdapter(adapterMainActivity);
+
+
+
+    }
+}
